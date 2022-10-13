f1372cebaca5982e19ed7fb004c608d6626a40c3
Simon Balleux <simoncestmoi@hotmail.fr>
2022-10-13 15:19:39 +0200

    Mise en place OK de ma structure et recuperation de donnée

--- Views/template.php
+++ Views/template.php
@@ -1,87 +1,34 @@
+
+
 <!DOCTYPE html>
-<html>
-
-<head>
-  <!-- Basic -->
-  <meta charset="utf-8" />
-  <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-  <!-- Mobile Metas -->
-  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
-  <!-- Site Metas -->
-  <meta name="keywords" content="" />
-  <meta name="description" content="" />
-  <meta name="author" content="" />
-
-  <title>
-    TripBiz
-  </title>
-
-  <!-- range selctor slider style -->
-
-
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/ion-rangeslider/2.3.0/css/ion.rangeSlider.min.css" />
-  <!-- bootstrap core css -->
-  <link rel="stylesheet" type="text/css" href="public/css/bootstrap.css" />
-  <!-- fonts style -->
-  <link href="https://fonts.googleapis.com/css?family=Poppins:400,600,700&display=swap" rel="stylesheet" />
-  <!-- Custom styles for this template -->
-  <link href="public/css/style.css" rel="stylesheet" />
-  <!-- responsive style -->
-  <link href="public/css/responsive.css" rel="stylesheet" />
-</head>
-
-<body>
-    <div class="hero_area">
-        <!-- header section strats -->
-        <header class="header_section">
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <meta name="description" content="" />
+        <meta name="author" content="" />
+        <title>Blog Post - Start Bootstrap Template</title>
+        <!-- Favicon-->
+        <link rel="icon" type="image/x-icon" href="public/assets/favicon.ico" />
+        <!-- Core theme CSS (includes Bootstrap)-->
+        <link href="public/css/styles.css" rel="stylesheet" />
+    </head>
+    <body>
+        <!-- Responsive navbar-->
+        <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
             <div class="container">
-                <nav class="navbar navbar-expand-lg custom_nav-container pt-3">
-                    <a class="navbar-brand" href="#">
-                    <span>
-                        TripBiz
-                    </span>
-                    </a>
-                    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
-                    </button>
-
-                    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <div class="d-flex ml-auto flex-column flex-lg-row align-items-center">
-                        <ul class="navbar-nav  ">
-                        <li class="nav-item active">
-                            <a class="nav-link" href="#">Home
-                            <span class="sr-only">(current)</span></a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="#bookTrip">
-                            Book Trip
-                            </a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="#package">
-                            Packages
-                            </a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="#service">Services</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="#blog">Blog</a>
-                        </li>
-                        </ul>
-                        <form class="form-inline my-2 my-lg-0 ml-0 ml-lg-4 mb-3 mb-lg-0">
-                        <button class="btn  my-2 my-sm-0 nav_search-btn" type="submit"></button>
-                        </form>
-                    </div>
-                    <div class="quote_btn-container ml-0 ml-lg-4 d-flex justify-content-center">
-                        <a href="">
-                        Get A quote
-                        </a>
-                    </div>
-                    </div>
-                </nav>
+                <a class="navbar-brand" href="#!">Start Bootstrap</a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+                        <li class="nav-item"><a class="nav-link" href="#">Home</a></li>
+                        <li class="nav-item"><a class="nav-link" href="#!">About</a></li>
+                        <li class="nav-item"><a class="nav-link" href="#!">Contact</a></li>
+                        <li class="nav-item"><a class="nav-link active" aria-current="page" href="#">Blog</a></li>
+                    </ul>
+                </div>
             </div>
-        </header>
+        </nav>
     
 
 
@@ -92,16 +39,15 @@
 
 
 
-        <!-- Footer -->
-        <!-- footer section -->
-        <section class=" footer_section">
-            <p>
-            Copyright &copy; 2021 All Rights Reserved By
-            <a href="https://html.design/">Free
-                Html Templates</a><br><p>
-                Distributed By
-                <a href="https://themewagon.com">Themewagon</a>
-                </p>
-            </p>
-        </section>
-        <!-- footer section -->
+
+
+        <!-- Footer-->
+        <footer class="py-5 bg-dark">
+            <div class="container"><p class="m-0 text-center text-white">Copyright &copy; Your Website 2022</p></div>
+        </footer>
+        <!-- Bootstrap core JS-->
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
+        <!-- Core theme JS-->
+        <script src="public/js/scripts.js"></script>
+    </body>
+</html>
